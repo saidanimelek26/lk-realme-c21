2de22b3705a73aa881e367de861d55a987ff1915
melek saidani <mbcmelek3@gmail.com>
2026-03-29 22:02:42 +0100

    Change DTB file reference in build.mk
    Updated the DTB file reference in the build process.

--- make/build.mk
+++ make/build.mk
@@ -19,7 +19,7 @@ $(OUTBIN): $(OUTELF)
 
 $(OUTELF)-dtb.img: $(OUTBIN)
 	@echo adding dtb: $@
-	$(NOECHO)cat $< $(LK_TOP_DIR)/dtb/$(PHONE_DTB) > $@
+	$(NOECHO)cat $< $(LK_TOP_DIR)/dtb/$(main_dtb_header) > $@
 
 $(OUTELF)-sign.img: $(OUTELF)-dtb.img
 	@mv $(OUTBIN) $(OUTELF)-bin.img
