625acfb32a00f0f0480b4d7236bcf50eb25e7719
melek saidani <mbcmelek3@gmail.com>
2026-06-24 14:05:54 -0700

    Update rules.mk

--- platform/mt6735/rules.mk
+++ platform/mt6735/rules.mk
@@ -1,7 +1,7 @@
 LOCAL_DIR := $(GET_LOCAL_DIR)
 
 ARCH    := arm
-ARM_CPU := cortex-a7
+ARM_CPU := cortex-a53
 CPU     := generic
 
 MMC_SLOT         := 1
@@ -128,8 +128,8 @@ endif
 MTK_MUSB := yes
 ifeq ($(MTK_MUSB), yes)
 INCLUDES += -I$(LK_TOP_DIR)/platform/common/mt_musb
-OBJS += \
-	$(LOCAL_DIR)/mt_musb/mt_musbphy.o
+MODULE_SRCS += \
+	$(LOCAL_DIR)/mt_musb/mt_musbphy.c
 endif
 
 ifeq ($(CFG_MTK_WDT_COMMON), yes)
@@ -152,89 +152,90 @@ INCLUDES += -I$(LOCAL_DIR)/include \
 
 INCLUDES += -I$(DRVGEN_OUT)/inc
 
-OBJS += \
-	$(LOCAL_DIR)/bitops.o \
-	$(LOCAL_DIR)/mt_gpio.o \
-	$(LOCAL_DIR)/mt_disp_drv.o \
-	$(LOCAL_DIR)/mt_gpio_init.o \
-	$(LOCAL_DIR)/mt_i2c.o \
-	$(LOCAL_DIR)/platform.o \
-	$(LOCAL_DIR)/uart.o \
-	$(LOCAL_DIR)/interrupts.o \
-	$(LOCAL_DIR)/timer.o \
-	$(LOCAL_DIR)/debug.o \
-	$(LOCAL_DIR)/boot_mode.o \
-	$(LOCAL_DIR)/load_image.o \
-	$(LOCAL_DIR)/atags.o \
-	$(LOCAL_DIR)/partition_setting.o \
-	$(LOCAL_DIR)/mt_get_dl_info.o \
-	$(LOCAL_DIR)/addr_trans.o \
-	$(LOCAL_DIR)/factory.o \
-	$(LOCAL_DIR)/mtk_key.o \
-	$(LOCAL_DIR)/mt_logo.o\
-	$(LOCAL_DIR)/mt_pmic_wrap_init.o\
-	$(LOCAL_DIR)/mmc_common_inter.o \
-	$(LOCAL_DIR)/mmc_core.o\
-	$(LOCAL_DIR)/mmc_test.o\
-	$(LOCAL_DIR)/msdc.o\
-	$(LOCAL_DIR)/msdc_dma.o\
-	$(LOCAL_DIR)/msdc_utils.o\
-	$(LOCAL_DIR)/msdc_irq.o\
-	$(LOCAL_DIR)/upmu_common.o\
-	$(LOCAL_DIR)/mt_pmic.o\
-	$(LOCAL_DIR)/mt6311.o\
-	$(LOCAL_DIR)/mt_rtc.o\
-	$(LOCAL_DIR)/ddp_manager.o\
-	$(LOCAL_DIR)/ddp_path.o\
-	$(LOCAL_DIR)/ddp_ovl.o\
-	$(LOCAL_DIR)/ddp_rdma.o\
-	$(LOCAL_DIR)/ddp_misc.o\
-	$(LOCAL_DIR)/ddp_info.o\
-	$(LOCAL_DIR)/ddp_dither.o\
-	$(LOCAL_DIR)/ddp_dump.o\
-	$(LOCAL_DIR)/ddp_dsi.o\
-	$(LOCAL_DIR)/primary_display.o\
-	$(LOCAL_DIR)/disp_lcm.o\
-	$(LOCAL_DIR)/ddp_pwm.o\
-	$(LOCAL_DIR)/pwm.o \
-	$(LOCAL_DIR)/mtk_auxadc.o\
-	$(LOCAL_DIR)/mt_dummy_read.o\
-	$(LOCAL_DIR)/mt_efuse.o
+# Convert all OBJS to MODULE_SRCS with source file extensions
+MODULE_SRCS += \
+	$(LOCAL_DIR)/bitops.c \
+	$(LOCAL_DIR)/mt_gpio.c \
+	$(LOCAL_DIR)/mt_disp_drv.c \
+	$(LOCAL_DIR)/mt_gpio_init.c \
+	$(LOCAL_DIR)/mt_i2c.c \
+	$(LOCAL_DIR)/platform.c \
+	$(LOCAL_DIR)/uart.c \
+	$(LOCAL_DIR)/interrupts.c \
+	$(LOCAL_DIR)/timer.c \
+	$(LOCAL_DIR)/debug.c \
+	$(LOCAL_DIR)/boot_mode.c \
+	$(LOCAL_DIR)/load_image.c \
+	$(LOCAL_DIR)/atags.c \
+	$(LOCAL_DIR)/partition_setting.c \
+	$(LOCAL_DIR)/mt_get_dl_info.c \
+	$(LOCAL_DIR)/addr_trans.c \
+	$(LOCAL_DIR)/factory.c \
+	$(LOCAL_DIR)/mtk_key.c \
+	$(LOCAL_DIR)/mt_logo.c \
+	$(LOCAL_DIR)/mt_pmic_wrap_init.c \
+	$(LOCAL_DIR)/mmc_common_inter.c \
+	$(LOCAL_DIR)/mmc_core.c \
+	$(LOCAL_DIR)/mmc_test.c \
+	$(LOCAL_DIR)/msdc.c \
+	$(LOCAL_DIR)/msdc_dma.c \
+	$(LOCAL_DIR)/msdc_utils.c \
+	$(LOCAL_DIR)/msdc_irq.c \
+	$(LOCAL_DIR)/upmu_common.c \
+	$(LOCAL_DIR)/mt_pmic.c \
+	$(LOCAL_DIR)/mt6311.c \
+	$(LOCAL_DIR)/mt_rtc.c \
+	$(LOCAL_DIR)/ddp_manager.c \
+	$(LOCAL_DIR)/ddp_path.c \
+	$(LOCAL_DIR)/ddp_ovl.c \
+	$(LOCAL_DIR)/ddp_rdma.c \
+	$(LOCAL_DIR)/ddp_misc.c \
+	$(LOCAL_DIR)/ddp_info.c \
+	$(LOCAL_DIR)/ddp_dither.c \
+	$(LOCAL_DIR)/ddp_dump.c \
+	$(LOCAL_DIR)/ddp_dsi.c \
+	$(LOCAL_DIR)/primary_display.c \
+	$(LOCAL_DIR)/disp_lcm.c \
+	$(LOCAL_DIR)/ddp_pwm.c \
+	$(LOCAL_DIR)/pwm.c \
+	$(LOCAL_DIR)/mtk_auxadc.c \
+	$(LOCAL_DIR)/mt_dummy_read.c \
+	$(LOCAL_DIR)/mt_efuse.c
 
 ifneq ($(CFG_MTK_WDT_COMMON),yes)
-OBJS += $(LOCAL_DIR)/mtk_wdt.o
+MODULE_SRCS += $(LOCAL_DIR)/mtk_wdt.c
 endif
 
 ifeq ($(MTK_SECURITY_SW_SUPPORT),yes)
-	OBJS += $(LOCAL_DIR)/img_auth_stor.o
+	MODULE_SRCS += $(LOCAL_DIR)/img_auth_stor.c
 endif
 ifneq ($(MACH_FPGA_LED_SUPPORT), yes)
-	OBJS += $(LOCAL_DIR)/mt_leds.o
+	MODULE_SRCS += $(LOCAL_DIR)/mt_leds.c
 endif
 # SETTING of USBPHY type
-#OBJS += $(LOCAL_DIR)/mt_usbphy_d60802.o
-#OBJS += $(LOCAL_DIR)/mt_usbphy_e60802.o
+#MODULE_SRCS += $(LOCAL_DIR)/mt_usbphy_d60802.c
+#MODULE_SRCS += $(LOCAL_DIR)/mt_usbphy_e60802.c
 
-OBJS += $(LOCAL_DIR)/mt_battery.o
+MODULE_SRCS += $(LOCAL_DIR)/mt_battery.c
 
 ifneq ($(MTK_EMMC_SUPPORT),yes)
-#	OBJS +=$(LOCAL_DIR)/mtk_nand.o
-#	OBJS +=$(LOCAL_DIR)/bmt.o
+#	MODULE_SRCS += $(LOCAL_DIR)/mtk_nand.c
+#	MODULE_SRCS += $(LOCAL_DIR)/bmt.c
 endif
 
 ifeq ($(MTK_USB2JTAG_SUPPORT),yes)
-OBJS += $(LOCAL_DIR)/mt_usb2jtag.o
+MODULE_SRCS += $(LOCAL_DIR)/mt_usb2jtag.c
 DEFINES += MTK_USB2JTAG_SUPPORT
 endif
 
 ifeq ($(MTK_MT8193_SUPPORT),yes)
-OBJS +=$(LOCAL_DIR)/mt8193_init.o
-OBJS +=$(LOCAL_DIR)/mt8193_ckgen.o
-OBJS +=$(LOCAL_DIR)/mt8193_i2c.o
+MODULE_SRCS += $(LOCAL_DIR)/mt8193_init.c
+MODULE_SRCS += $(LOCAL_DIR)/mt8193_ckgen.c
+MODULE_SRCS += $(LOCAL_DIR)/mt8193_i2c.c
 endif
 
 ifeq ($(MTK_KERNEL_POWER_OFF_CHARGING),yes)
-OBJS +=$(LOCAL_DIR)/mt_kernel_power_off_charging.o
+MODULE_SRCS += $(LOCAL_DIR)/mt_kernel_power_off_charging.c
 DEFINES += MTK_KERNEL_POWER_OFF_CHARGING
 endif
 
@@ -243,20 +244,20 @@ DEFINES += MTK_PUMP_EXPRESS_SUPPORT
 endif
 
 ifeq ($(MTK_BQ24261_SUPPORT),yes)
-OBJS +=$(LOCAL_DIR)/bq24261.o
+MODULE_SRCS += $(LOCAL_DIR)/bq24261.c
 else
     ifeq ($(MTK_BQ24296_SUPPORT),yes)
-        OBJS +=$(LOCAL_DIR)/bq24296.o
+        MODULE_SRCS += $(LOCAL_DIR)/bq24296.c
     else
         ifeq ($(MTK_NCP1854_SUPPORT),yes)
-            OBJS +=$(LOCAL_DIR)/ncp1854.o
+            MODULE_SRCS += $(LOCAL_DIR)/ncp1854.c
         endif
     endif
 endif
 
 ifeq ($(DUMMY_AP),yes)
-OBJS +=$(LOCAL_DIR)/dummy_ap.o
-#OBJS +=$(LOCAL_DIR)/spm_md_mtcmos.o
+MODULE_SRCS += $(LOCAL_DIR)/dummy_ap.c
+#MODULE_SRCS += $(LOCAL_DIR)/spm_md_mtcmos.c
 endif
 
 ifeq ($(MTK_EFUSE_WRITER_SUPPORT), yes)
@@ -280,7 +281,7 @@ ifeq ($(MTK_DM_VERITY_OFF),yes)
 DEFINES += MTK_DM_VERITY_OFF
 endif
 
-OBJS += $(LOCAL_DIR)/dm_verity_status.o
+MODULE_SRCS += $(LOCAL_DIR)/dm_verity_status.c
 
 MTK_RC_TO_VENDOR ?= yes
 
